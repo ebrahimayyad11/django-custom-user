5e029287b1c913d2393d81195e6426c0a62f365c
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-07-14 22:16:37 +0300

    doing lab-29

---
+++ custom_user/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(('email address'), unique=True)
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
